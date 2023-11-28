20dd59c2823e84323b81b103aee3924e4fbdbce0
ahmedansari153 <16545540+ahmedansari153@users.noreply.github.com>
2023-11-28 09:45:28 -0600

    wrap in try catch

--- data/pa11y-title-collect.js
+++ data/pa11y-title-collect.js
@@ -6,32 +6,30 @@ const puppeteer = require('puppeteer');
 
 client.tasks.get({}, function (err, tasks){
     return new Promise(async (resolve, reject) => {
-        try {
+    try {
             const browser = await puppeteer.launch();
             const page = await browser.newPage();
+            for(var t of tasks) {
+                let url = t.name;
+                let id = t.id;
+                await page.goto(url, {waitUntil: 'load', timeout: 0});
+                
+                let title = await page.evaluate(() => {
+                        return document.querySelector("h1.article-title").getInnerHTML();
+                }); 
+
+                if (title.length > 0) {
+                    client.task(id).edit({
+                        name: title
+                    }, function (err, task) {
+                        // task  =  object representing the newly updated task, or null if an error occurred
+                    });
+                }
+            }
+            browser.close();
+            
         } catch (error) {
             console.log(error);
         }
-        for(var t of tasks) {
-            let url = t.name;
-            let id = t.id;
-            await page.goto(url, {waitUntil: 'load', timeout: 0});
-            try {
-                let title = await page.evaluate(() => {
-                    return document.querySelector("h1.article-title").getInnerHTML();
-               });  
-            } catch (error) {
-                console.log("Unable to get title for: ", url);
-                console.log(error);
-            }
-            if (title.length > 0) {
-                client.task(id).edit({
-                    name: title
-                }, function (err, task) {
-                    // task  =  object representing the newly updated task, or null if an error occurred
-                });
-            }
-        }
-        browser.close();
     });
 });
