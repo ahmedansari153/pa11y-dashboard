ed211848f4da03556ac26035dc7b9cdd860ed564
ahmedansari153 <16545540+ahmedansari153@users.noreply.github.com>
2023-11-28 08:46:44 -0600

    fix syntax error

--- data/pa11y-title-collect.js
+++ data/pa11y-title-collect.js
@@ -4,24 +4,23 @@ const config = require('../config');
 const client = createClient('http://' + config.webservice.host + ':' + config.webservice.port + '/');
 const puppeteer = require('puppeteer');
 
-let allTasks = client.tasks.get
-allTasks({}, function (err, tasks){
-        return new Promise(async (resolve, reject) => {
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            for(var t of allTasks) {
-                let url = t.name;
-                let id = t.id;
-                await page.goto(url);
-                let title = document.querySelector("h1.article-title").getInnerHTML();
-                if (title.length > 0) {
-                    client.task(id).edit({
-                        name: title
-                    }, function (err, task) {
-                        // task  =  object representing the newly updated task, or null if an error occurred
-                    });
-                }
+client.tasks.get({}, function (err, tasks){
+    return new Promise(async (resolve, reject) => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        for(var t of tasks) {
+            let url = t.name;
+            let id = t.id;
+            await page.goto(url);
+            let title = document.querySelector("h1.article-title").getInnerHTML();
+            if (title.length > 0) {
+                client.task(id).edit({
+                    name: title
+                }, function (err, task) {
+                    // task  =  object representing the newly updated task, or null if an error occurred
+                });
             }
-            browser.close();
-        });
+        }
+        browser.close();
+    });
 });
